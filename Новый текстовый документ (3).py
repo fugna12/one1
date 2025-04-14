119162a6dd0c0ec9078e70fdb3a3e08dcfe40a27
fugna52 <daniildomasnij50@gmail.com>
2025-04-14 18:39:14 +0300

    Update Новый текстовый документ (3).py

---
+++ Новый текстовый документ (3).py
@@ -0,0 +1,27 @@
+#подключаем модуль с направляющими линиями
+from PyQt5.QtCore import Qt
+#подключаем необходимые виджеты
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout
+
+
+#создаём объект приложения
+app = QApplication([])
+# создаём объект главного окна
+my_win = QWidget()
+# создаём название главного окна
+my_win.setWindowTitle('Моё первое приложение')
+# задаём точку появления окна на экране компьютера
+my_win.move(900, 70)
+# задаём размер окна
+my_win.resize(400, 200)
+# даём команду окну показаться
+my_win.show()
+# создаём направляющую горизонтальную линию
+line = QHBoxLayout()
+# создаём объект текста
+title = QLabel('Hello, world!')
+# помещаем текст на направляющую линию по центру
+line.addWidget(title, alignment=Qt.AlignCenter)
+# добавляем получившуюся линию на окно приложения ZZZZZZZZZZZZZZOV
+my_win.setLayout(line)
+app.exec_()
